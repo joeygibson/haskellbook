8b52ce26b8bba53567961f5c81e2918c417255e2
Joey Gibson <joey@joeygibson.com>
2017-06-26 23:45:39 -0400

    ch 4.10

--- src/types.hs
+++ src/types.hs
@@ -1,6 +1,34 @@
 module Types where
 
-data Mood = Blah | Woot deriving Show
+data Mood
+  = Blah
+  | Woot
+  deriving (Show)
 
 changeMood Blah = Woot
 changeMood _ = Blah
+
+greetIfCool :: String -> IO ()
+greetIfCool coolness =
+  if cool
+    then putStrLn "Cool"
+    else putStrLn "Square"
+  where
+    cool = coolness == "frosty"
+
+gicWithFunction :: String -> IO ()
+gicWithFunction coolness =
+  if cool coolness
+    then putStrLn "Cool"
+    else putStrLn "Square"
+  where
+    cool v = v == "frosty"
+
+isPalindrome :: (Eq a) => [a] -> Bool
+isPalindrome str = (reverse str) == str
+
+myAbs :: Integer -> Integer
+myAbs n =
+  if n < 0
+    then n - (2 * n)
+    else n
