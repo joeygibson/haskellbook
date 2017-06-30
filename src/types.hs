99a7ae48092ca592bfc085c6636b0303d846dbf2
Joey Gibson <joey@joeygibson.com>
2017-06-29 22:56:59 -0400

    finished ch5

--- src/types.hs
+++ src/types.hs
@@ -32,3 +32,35 @@ myAbs n =
   if n < 0
     then n - (2 * n)
     else n
+
+-- partial application
+addStuff :: Integer -> Integer -> Integer
+addStuff a b = a + b + 5
+
+addTen = addStuff 5
+
+subtractStuff :: Integer -> Integer -> Integer
+subtractStuff x y = x - y - 10
+
+subtractOne = subtractStuff 1
+
+-- decurrying and desugaring...?
+nonsense :: Bool -> Integer
+nonsense True = 805
+nonsense False = 31337
+
+curriedFunction :: Integer -> Bool -> Integer
+curriedFunction i b = i + (nonsense b)
+
+uncurriedFunction :: (Integer, Bool) -> Integer
+uncurriedFunction (i, b) = i + (nonsense b)
+
+anonymous :: Integer -> Bool -> Integer
+anonymous = \i b -> i + (nonsense b)
+
+anonNested :: Integer -> Bool -> Integer
+anonNested = \i -> \b -> i + (nonsense b)
+
+-- type inference
+f :: Num a => a -> a -> a
+f x y = x + y + 3
